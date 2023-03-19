737246179f27d3f0e548c7d7bde8df6d01dc3236
Matthew O'Leary <matthewoleary@Matthews-MacBook-Pro.local>
2023-03-18 20:35:32 -0700

    first

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`_src${src}`, {
+    widths: [300, 800, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "_site/images/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("_src/css/");
+    eleventyConfig.addWatchTarget("_src/css/");
+    eleventyConfig.addPassthroughCopy("_src/images/");
+    eleventyConfig.addPassthroughCopy({ "_src/favicons": "/" });
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
+
+    return {
+        dir: {
+            input: "_src",
+            output: "_site"
+        }
+        };
+    };
+  
